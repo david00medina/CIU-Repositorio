9ccbb28d6af58f9760e62a8181b42f926d45e9eb
David Alberto Medina Medina <david00medina@gmail.com>
2019-04-14 15:47:18 +0100

    [+] Generating a frequency modulation depending on the amplitude of a sine carrier wave

--- Practica9/Practica9.ino
+++ Practica9/Practica9.ino
@@ -1,29 +1,32 @@
-const int PIN_LED = 13;
-const byte interruptPin = 2;
-volatile byte state = LOW;
-float sinVal = 0.f;
-const float frequency  = 0.1f;
+#include <Arduino.h>
+
+#define A                       1.f
+#define A_MIN                   -A
+#define F                       1.f
+#define W                       2.f * PI
+
+#define carrier_wave(t)         ( A * sin(W * F * t) )
+#define freq_factor(y)          ( 1.f + (y - A_MIN) )
+#define new_freq(y)             ( F * freq_factor(y) )
+#define generated_wave(y, t)    ( A * sin(W * new_freq(y) * t) )
+
+float x = 0.f;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(PIN_LED, OUTPUT);
-  pinMode ( interruptPin , INPUT_PULLUP) ;
-  
-
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   float t = millis() / 1000.f;
-  float f = frequency * sinVal / 255;
-  
-  float w = 2.f * PI * frequency;
- 
-  sinVal = 128.f + 128.f * sin(w * t);
-  //analogWrite(PIN_LED, sinVal);
-  digitalWrite(PIN_LED,HIGH);
-  delay(sinVal);
-  digitalWrite(PIN_LED,LOW);
-  delay(sinVal);
-  Serial.println(sinVal);
+
+  x = carrier_wave(t);
+  Serial.println(new_freq(x));
+  float y = generated_wave(x, t);
+
+  if(y >= 0.f) {
+      digitalWrite(LED_BUILTIN, HIGH);
+  } else if (y < 0.f) {
+      digitalWrite(LED_BUILTIN, LOW);
+  }
 }
